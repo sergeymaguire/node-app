93510c7fddbcd99c592061e4efa5447737e66177
sergeymaguire <sergeymaguire@gmail.com>
2018-10-16 09:02:09 -0700

    A node app

--- liri.js
+++ liri.js
@@ -2,15 +2,14 @@
 function helpMenu() {
     console.log("Please one of the following... " + "\n" + "A movie name(node liri.js movie-this<Movie name here>)" + "\n" + "A song name(node liri.js spotify-this-song<Song name here)" + "\n" + "A band lookup(node liri.js concert-this<artist/band name here>)");
 }
-//const fs = require('fs');
+
 var request = require("request"),
     fs = require("fs"),
     keys = require("./keys.js"),
     infoInput = process.argv,
     action = process.argv[2],
     title = "",
-    artist = "";
-
+  
 if (!action || !infoInput || !infoInput.length) {
     helpMenu();
     process.exit(1);
@@ -21,7 +20,6 @@ if (process.argv[3] !== undefined) {
         title += infoInput[i] + " ";
     };
 }
-
 
 switch (action) {
     // case 'concert-this':
@@ -41,38 +39,19 @@ switch (action) {
 function movie() {
     var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
     request(queryURL, function (error, response, body) {
-
         if (!error && response.statusCode === 200) {
             if (body) {
                 var data = JSON.parse(body);
                 if (data.Error == 'Movie not found!') {
-                    
-
-                    fs.appendFile('log.txt', 'data to append', function (err) {
-                        if (err) throw err;
-                        console.log('Saved!');
-                    });
-                    console.log("\n********************************** MOVIE THIS **********************************\nOMDB could not find any movies that matched that title.  Please try again.\n********************************************************************************\n");
-
+                    var movieAppend = ("\n********************************** MOVIE THIS **********************************\nOMDB could not find any movies that matched that title.  Please try again.\n********************************************************************************\n");
+                    console.log(movieAppend)
                 } else if (!data.Ratings || data.Ratings.length < 2) {
-                   
-
-                    fs.appendFile('log.txt', 'data to append', function (err) {
-                        if (err) throw err;
-                        console.log('Saved!');
-                    });
-                    console.log("\n********************************** MOVIE THIS **********************************\nTitle: " + data.Title + "\nRelease Year: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: No Rotten Tomatoes Rating\nCountry movie produced in: " + data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors + "\n********************************************************************************\n");
-
+                    var movieAppend = ("\n********************************** MOVIE THIS **********************************\nTitle: " + data.Title + "\nRelease Year: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: No Rotten Tomatoes Rating\nCountry movie produced in: " + data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors + "\n********************************************************************************\n");
+                    console.log(movieAppend)
                     return
                 } else if (data.Ratings[1].Value !== undefined) {
-                    
-
-                    fs.appendFile('log.txt', 'data to append', function (err) {
-                        if (err) throw err;
-                        console.log('Saved!');
-                    });
-                    console.log("\n********************************** MOVIE THIS **********************************\nTitle: " + data.Title + "\nRelease Year: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value + "\nCountry movie produced in: " + data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors + "\n********************************************************************************\n");
-
+                    var movieAppend = ("\n********************************** MOVIE THIS **********************************\nTitle: " + data.Title + "\nRelease Year: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value + "\nCountry movie produced in: " + data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors + "\n********************************************************************************\n");
+                    console.log(movieAppend)
                 };
             };
         };
