dc99f7a091d42d9cf1def185705d9875ed0edc5f
sergeymaguire <sergeymaguire@gmail.com>
2018-10-17 14:45:14 -0700

    A node application

--- liri.js
+++ liri.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 function helpMenu() {
-    console.log( "\n" + "How to run the lookup commands... " + "\n"  +"\n" + "A movie name(node liri.js movie-this<Movie name here>)" + "\n" + "A song name(node liri.js spotify-this-song<Song name here)" + "\n" + "A band lookup(node liri.js concert-this<artist/band name here>" + "\n");
+    console.log( "\n" + "How to run the lookup commands... " + "\n"  +"\n" + "A movie name(node liri.js movie-this<Movie name here>)" + "\n" + "A song name(node liri.js spotify-this-song<Song name here)" + "\n" + "A band lookup(node liri.js concert-this<artist/band name here>" + "\n" + "Do what it says(node liri.js do-what-it-says<Will do one of the three commands above depending on what is on your random.txt file>)" + "\n");
 }
 helpMenu();
+const fs = require("fs");
 var request = require("request"),
-    fs = require("fs"),
     keys = require("./keys.js"),
     Spotify = require('node-spotify-api'),
     spotify = new Spotify({
@@ -24,6 +24,10 @@ if (process.argv[3] !== undefined) {
         param += infoInput[i] + " ";
     };
 };
+
+    fs.readFile('random.txt', "utf8", function(err, data) {
+        console.log(data)
+    });
 
 
 switch (action) {
